2f7deed42774465a0f4f543f5dfa9775d029fb51
Iryna <pysarenkoiryna@gmail.com>
2022-06-28 09:45:10 +0300

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,14 +9,15 @@ const daysSpanEl = document.querySelector('span[data-days]');
 const hoursSpanEl = document.querySelector('span[data-hours]');
 const minutesSpanEl = document.querySelector('span[data-minutes]');
 const secondsSpanEl = document.querySelector('span[data-seconds]');
-
+startBtn.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0].getTime());
+    startBtn.disabled = false;
+
     if (selectedDates[0].getTime() > Date.now()) {
       setInterval(() => {
         const startTime = convertMs(selectedDates[0] - Date.now());
@@ -27,11 +28,7 @@ const options = {
       }, 1000);
       return (startBtn.disabled = false);
     } else {
-      userDateEl.textContent = String('00');
-      daysSpanEl.textContent = String('00');
-      hoursSpanEl.textContent = String('00');
-      minutesSpanEl.textContent = String('00');
-      secondsSpanEl.textContent = String('00');
+      userDateElюdisabled = true;
       alert('Please choose a date in the future');
       clearInterval();
       return (startBtn.disabled = true);
@@ -40,19 +37,6 @@ const options = {
 };
 
 flatpickr(userDateEl, options);
-
-// function timerStart(options) {
-//   setInterval(() => {}, 1000);
-// }
-
-// startBtn.addEventListener('click', () => {
-//   timerId = setInterval(flatpickr => {
-//     daysSpanEl.textContent = startTime.days.currentTarget;
-//     hoursSpanEl.textContent = startTime.hours;
-//     minutesSpanEl.textContent = startTime.minutes;
-//     secondsSpanEl.textContent = startTime.seconds;
-//   }, 1000);
-// });
 
 function pad(value) {
   return String(value).padStart(2, '0');
@@ -76,22 +60,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-// //     secondsSpanEl.textContent = components.seconds;
-// //   const currentTime = Date.now();
-// //   //   const different = userTime - currentTime;
-// //   const userTime = convertMs(
-// //     options.defaultDate.getTime() - currentTime.getTime()
-// //   );
-// //   daysSpanEl.textContent = userTime.days;
-// //   setInterval(() => {
-// //     // const currentTime = Date.now();
-// //     // const different = userTime - currentTime;
-// //     // console.log(different);
-
-// //     const components = convertMs(different);
-// //     // daysSpanEl.textContent = components.days;
-// //     hoursSpanEl.textContent = components.hours;
-// //     minutesSpanEl.textContent = components.minutes;
-// //     secondsSpanEl.textContent = components.seconds;
-// //     console.log(components);
